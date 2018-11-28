661f50c5df11a7e0bab1e2207bf29adfe0b5b83f
matttali09 <matttaliancich09@gmail.com>
2018-11-28 14:18:09 -0500

    added a favorites html and fixed results nav bar

--- assets/JavaScript/favorites.js
+++ assets/JavaScript/favorites.js
@@ -2,18 +2,18 @@
 let favorites = [];
 
 
-
 // favorites logic
 $(this).on("click", ".favorites", function(event) {
     event.preventDefault();
-    var favsSRC = $(this).attr("src")
+    $(this).css("color: lightgrey")
+    var favsSRC = $(this).attr("data-value")
     var favsDiv = $("<div>")
     favsDiv.text($(this).val())
     favsDiv.attr(favsSRC);
-    $("#favorites").append(favsDiv);
+    // $("#favorites").append(favsDiv);
 });
 
-$(favsDiv).click(function(e) {
-    e.preventDefault();
-    window.open(favsDiv.attr("src"))
-})
+// $(favsDiv).click(function(e) {
+//     e.preventDefault();
+//     window.open(favsDiv.attr("src"))
+// })
