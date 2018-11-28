a48777c7d0e16173170a6d0d12b471f187a8ac36
Winfield H <winterfreshness@gmail.com>
2018-11-27 22:29:34 -0500

    Get results from both API's and display on the page. Added GPS capability

--- assets/JavaScript/ajax.js
+++ assets/JavaScript/ajax.js
@@ -13,15 +13,15 @@ let thirdIng = "";
 let fourthIng = "";
 let fifthIng = "";
 let sixthIng = "";
-let lat;
-let long;
+let myLat;
+let myLong;
 let address = "3024+Meadow+Lake+Ave.,+Largo,+FL";
 let cuisine = "";
 
 function restaurantAjax() {
     let clientID = "2EQ443BHONMJJ0ZGUNR4ZWXOJQPGBRWDWVV55UBLPSOS5B3E";
     let clientSecret = "YUCEUTQGZR3IAVPWFSB1C1ICQSKO1ABUBFEIKCRSWXMHVZQJ";
-    let queryURL = "https://api.foursquare.com/v2/venues/explore?client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20181127&limit=5&ll=" + lat + "," + long + "&query=" + mainIng;
+    let queryURL = "https://api.foursquare.com/v2/venues/explore?client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20181127&limit=5&ll=" + myLat + "," + myLong + "&query='" + mainIng + "'";
 
     $.ajax({
         url: queryURL,
@@ -32,7 +32,37 @@ function restaurantAjax() {
         console.log(queryURL);
         console.log(resp);
 
+        displayRestaurants(resp.response.groups["0"].items);
     });
+}
+
+function displayRestaurants(data) {
+    // confirm the data exists first
+    console.table(data);
+
+    // dynamically create the content for each result
+    for (let i = 0; i < 5; i++) {
+        // get values for the listing
+        let name = data[i].venue.name;
+        let venueID = data[i].venue.id;
+        let address = data[i].venue.location.formattedAddress;
+        let lat = data[i].venue.location.lat;
+        let long = data[i].venue.location.lng;
+
+        // create the content for the restaurtant
+        let list = $("<li>").addClass("collection-item avatar");
+        let link = $("<a>").attr("href", "#").attr("id", venueID).attr("data-lat", lat).attr("data-long", long);
+        let icon = $("<i>").addClass("material-icons circle green").text("map");
+        let title = $("<span>").addClass("title").html("<strong>" + name + "</strong>");
+        let addr = $("<p>").html(address[0] + "<br>" + address[1] + "<br>" + address[2]);
+
+        // combine elements and add to the DOM
+        link.append(icon);
+        list.append(link);
+        list.append(title);
+        list.append(addr);
+        $("#restaurant-results").append(list);
+    }
 }
 
 function recipeAjax() {
@@ -50,16 +80,15 @@ function recipeAjax() {
         console.log(resp);
 
         // add the results to the HTML page
-        displayOnPage(resp.recipes);
+        displayRecipes(resp.recipes);
     });
 }
 
-function displayOnPage(data) {
+function displayRecipes(data) {
     console.table(data);
     for (let i = 0; i < 5; i++) {
         // make the card div
         let card = $("<div>").addClass("card");
-        console.log(`I'm making a card. i is: ${i}`);
 
         // make the div for the image
         let cardImage = $("<div>").addClass("card-image waves-effect waves-block waves-light");
@@ -70,16 +99,8 @@ function displayOnPage(data) {
         let cardContent = $("<div>").addClass("card-content");
         let title = $("<p>").addClass("card-title activator grey-text text-darken-4").text(data[i].title);
         let link = $("<a>").attr("href", data[i].source_url).text("Click here!");
-        cardContent.append(title, link);
-
-        // make the hidden card revealed info
-        // let cardReveal = $("<div").addClass("card-reveal");
-        // let title2 = $("<span>").addClass("card-title grey-text text-darken-4").text(data[i].title);
-        // let exitIcon = $("<i>").addClass("material-icons right").text("more_vert");
-        // 
-        // title2.append(exitIcon);
-        // cardReveal.append(title);
-        // cardReveal.append(link);
+        cardContent.append(title);
+        cardContent.append(link);
 
         // put the content on the DOM
         card.append(cardImage);
@@ -92,19 +113,15 @@ function displayOnPage(data) {
 // function that checks to see if GPS capability is available
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(function (position) {
+            // set the values of the location
+            myLat = parseFloat(position.coords.latitude);
+            myLong = parseFloat(position.coords.longitude);
+            $("#coord").html("Latitude: " + myLat + "<br>Longitude: " + myLong);
+        });
     } else {
         $("#coord").text("Geolocation is not supported by this browser.");
     }
-}
-
-// if GPS is available, it saves the latitude and longitude
-function showPosition(position) {
-    // set the values of the location
-    lat = parseFloat(position.coords.latitude);
-    long = parseFloat(position.coords.longitude);
-    $("#coord").html("Latitude: " + lat +
-        "<br>Longitude: " + long);
 }
 
 // document ready func.
@@ -124,7 +141,7 @@ $(function () {
         // cuisine = $("#cuisine").val().trim();
         // dietRest = $("#diet-rest").val().trim();
         restaurantAjax();
-        //recipeAjax();
+        recipeAjax();
     });
 
     $("#loc").on("click", getLocation);
