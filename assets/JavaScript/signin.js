1c6ae47a1e4d00aa1c8303321ce2fd180b09a7d9
matttali09 <matttaliancich09@gmail.com>
2018-11-27 23:21:11 -0500

    comment

--- assets/JavaScript/signin.js
+++ assets/JavaScript/signin.js
@@ -2,31 +2,31 @@ console.log("this is before anything");
 
 function onSignIn(googleUser) {
   console.log("this is after signin press");
-  
-    var profile = googleUser.getBasicProfile();
-    console.log(profile);
-    
-    $(".g-signin2").css("display", "none");
-    $(".data").css("display", "block");
-    $("#pic").attr('src', profile.getImageUrl());
-    $("#emailAdd").text(profile.getEmail());
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
 
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        alert("You have successfully signed out")
-        $(".g-signin2").css("display", "block");
-        $(".data").css("display", "none");
-      console.log('User signed out.');
-    });
-  }
+  var profile = googleUser.getBasicProfile();
+  console.log(profile);
 
-  $("#go-back").click(function(e) {
-    e.preventDefault();
-    window.open("index.html")
+  $(".g-signin2").css("display", "none");
+  $(".data").css("display", "block");
+  $("#pic").attr('src', profile.getImageUrl());
+  $("#emailAdd").text(profile.getEmail());
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    alert("You have successfully signed out")
+    $(".g-signin2").css("display", "block");
+    $(".data").css("display", "none");
+    console.log('User signed out.');
   });
+}
+
+$("#go-back").click(function (e) {
+  e.preventDefault();
+  window.open("index.html")
+});
